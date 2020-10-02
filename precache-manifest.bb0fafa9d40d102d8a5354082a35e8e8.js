a1cbd932df8811c535f031d6aea16d11063a667a
DamjanKarlovski <damjan_karlovski@hotmail.com>
2020-10-02 10:19:56 +0200

    Updates

---
+++ precache-manifest.bb0fafa9d40d102d8a5354082a35e8e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097fe0689a58680e442cd92b3e5b6136",
+    "url": "/Face-Recognition-App/index.html"
+  },
+  {
+    "revision": "675382724e0921f9d18d",
+    "url": "/Face-Recognition-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a9c10735c70b2121d446",
+    "url": "/Face-Recognition-App/static/css/main.8a1fadda.chunk.css"
+  },
+  {
+    "revision": "675382724e0921f9d18d",
+    "url": "/Face-Recognition-App/static/js/2.365dd1c0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-Recognition-App/static/js/2.365dd1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c10735c70b2121d446",
+    "url": "/Face-Recognition-App/static/js/main.1feea119.chunk.js"
+  },
+  {
+    "revision": "1e39da3ab3ac0d344705",
+    "url": "/Face-Recognition-App/static/js/runtime-main.516bb369.js"
+  },
+  {
+    "revision": "4100098c8af8b04589771aa58935f4c8",
+    "url": "/Face-Recognition-App/static/media/brain.4100098c.png"
+  }
+]);
